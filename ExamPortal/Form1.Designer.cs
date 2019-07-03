63d996cd6b0e032088a103c6387b7d4514e25681
Rudra Malali <rudra.malali@gmail.com>
2019-07-03 23:41:30 +0530

    Add project files.

---
+++ ExamPortal/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ExamPortal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 62);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(103, 110);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(568, 258);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Answers:-";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(18, 211);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(17, 16);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(18, 154);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(17, 16);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(18, 94);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(17, 16);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 36);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(17, 16);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 524);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ ExamPortal/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ExamPortal
+{
+    
+    public partial class Form1 : Form
+    {
+        string connstring;
+        SqlConnection con;
+        SqlCommand command;
+        SqlDataAdapter da = new SqlDataAdapter();
+        SqlDataReader dr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            global.marks = 0;
+            connstring = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=testrud";
+            con = new SqlConnection(connstring);
+            con.Open();
+            command = new SqlCommand("UPDATE quiz SET flag = 0", con);
+            da.UpdateCommand = new SqlCommand("UPDATE quiz SET flag = 0", con);
+            da.UpdateCommand.ExecuteNonQuery();
+            da.Dispose();
+            command = new SqlCommand("SELECT TOP 1 * FROM quizview ORDER BY NEWID()", con);
+            dr = command.ExecuteReader();
+            while (dr.Read())
+            {
+                label1.Text = Convert.ToString(dr.GetValue(1));
+                radioButton1.Text = Convert.ToString(dr.GetValue(2));
+                radioButton2.Text = Convert.ToString(dr.GetValue(3));
+                radioButton3.Text = Convert.ToString(dr.GetValue(4));
+                radioButton4.Text = Convert.ToString(dr.GetValue(5));
+                global.sopt = Convert.ToString(dr.GetValue(0));
+
+
+            }
+            dr.Close();
+            da.UpdateCommand = new SqlCommand("UPDATE quiz SET flag = 1 where id=" +global.sopt, con);
+            da.UpdateCommand.ExecuteNonQuery();
+            da.Dispose();
+
+            
+            command.Dispose();
+            con.Close();
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                global.opt = 1;
+                radioButton1.Checked = false;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                global.opt = 2;
+                radioButton2.Checked = false;
+            }
+            else if (radioButton3.Checked == true)
+            {
+                global.opt = 3;
+                radioButton3.Checked = false;
+            }
+            else if (radioButton4.Checked == true)
+            {
+                global.opt = 4;
+                radioButton4.Checked = false;
+            }
+            else
+                global.opt = 99;
+        
+            con.Open();
+            string qvar = label1.Text;
+            string q = "SELECT * FROM quiz where question = '" + qvar+"'";
+            command = new SqlCommand(q, con);
+            dr = command.ExecuteReader();
+            
+                while (dr.Read())
+                {
+                    if (global.opt == Convert.ToInt32(dr.GetValue(6)))
+                        global.marks += 1;
+                }
+                dr.Close();
+                command.Dispose();
+
+                command = new SqlCommand("SELECT TOP 1 * FROM quizview where flag=0 ORDER BY NEWID()", con);
+                dr = command.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    label1.Text = Convert.ToString(dr.GetValue(1));
+                    radioButton1.Text = Convert.ToString(dr.GetValue(2));
+                    radioButton2.Text = Convert.ToString(dr.GetValue(3));
+                    radioButton3.Text = Convert.ToString(dr.GetValue(4));
+                    radioButton4.Text = Convert.ToString(dr.GetValue(5));
+                    global.sopt = Convert.ToString(dr.GetValue(0));
+                }
+                dr.Close();
+                da.UpdateCommand = new SqlCommand("UPDATE quiz SET flag = 1 where id=" + global.sopt, con);
+                da.UpdateCommand.ExecuteNonQuery();
+                da.Dispose();
+
+
+                command.Dispose();
+                
+            }
+            else
+            {
+                button1.Text = Convert.ToString(global.marks);
+            }
+            con.Close();
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    static class global
+    {
+        public static int opt, marks;
+        public static string sopt;
+    }
+}
